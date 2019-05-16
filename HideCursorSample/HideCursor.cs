b2f7fa0080a21cc690cb70df2e4fd49732935c77
yasuo <pambox+github@gmail.com>
2019-05-16 23:50:03 +0900

    プロジェクト ファイルを追加します。

---
+++ HideCursorSample/HideCursor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace HideCursorSample
+{
+    public class HideCursor : IDisposable
+    {
+        private readonly DispatcherTimer cursorTimer;
+        public HideCursor(int interval = 5)
+        {
+            cursorTimer = new DispatcherTimer();
+            cursorTimer.Tick += new EventHandler(CursorTimer_Tick);
+            cursorTimer.Interval = new TimeSpan(0, 0, interval);
+            cursorTimer.Start();
+            Mouse.OverrideCursor = Cursors.None;
+        }
+
+        public void Dispose()
+        {
+            cursorTimer.Stop();
+            Mouse.OverrideCursor = Cursors.Arrow;
+        }
+
+        public void MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Mouse.OverrideCursor == Cursors.None)
+            {
+                Mouse.OverrideCursor = Cursors.Arrow;
+            }
+            cursorTimer.Stop();
+            cursorTimer.Start();
+        }
+        private void CursorTimer_Tick(object sender, EventArgs e)
+        {
+            Mouse.OverrideCursor = Cursors.None;
+        }
+    }
+}
---
+++ HideCursorSample/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace HideCursorSample
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly HideCursor hideCursor = new HideCursor();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.MouseMove += new MouseEventHandler(hideCursor.MouseMove);
+        }
+    }
+}
